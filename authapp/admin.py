102fa52c9403eaa035e33273300e0c63fa870e0b
aswinpradeepc <aswinpradeepc@gmail.com>
2025-03-20 18:48:19 +0530

    Unregister unnecessary models from the admin interface for cleaner management

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken, EmailAddress
+from django.contrib.auth.models import Group
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 
-# Register your models here.
+# Unregister models you don't want to see in the admin
+admin.site.unregister(Site)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialToken)
+admin.site.unregister(Group)
+admin.site.unregister(OutstandingToken)
+admin.site.unregister(BlacklistedToken)
+admin.site.unregister(EmailAddress)
